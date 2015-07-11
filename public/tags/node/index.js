aa338cfa6abb485afad8c0ccdb3d8c4660f63c9d
Henrique Dias <hacdias@gmail.com>
2015-07-11 12:23:08 +0100

    updates

--- public/tags/node/index.js
+++ public/tags/node/index.js
@@ -1,9 +1,9 @@
 <div>
         <ul id="list">
             
+            How to create a Node.js command-line application
             
-            
-            
+            Como utilizar o Bower, um gestor de pacotes web
             
         </ul>
   </div>
